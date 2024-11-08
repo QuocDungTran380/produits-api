bb613a04caac367998a3c2580a70c864ef384d3e
QuocDungTran380 <121247710+QuocDungTran380@users.noreply.github.com>
2024-11-08 14:08:58 -0500

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env?.PORT || "",
+  port: process.env?.PORT || 8080,
   JWT_SECRET: process.env.JWT_SECRET || ""
 };
