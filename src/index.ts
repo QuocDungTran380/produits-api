0983fb60fd20d73e06d98d3487ceb47ec87db179
QuocDungTran380 <121247710+QuocDungTran380@users.noreply.github.com>
2024-11-08 13:53:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { config } from "./utils/config";
 const port = config.port;
 
 // Démarrage du serveur
-httpApp.listen(port, '0.0.0.0' => {
+httpApp.listen(port, () => {
   console.log(`Serveur en écoute sur <https://localhost>:${port}`);
 });
 
